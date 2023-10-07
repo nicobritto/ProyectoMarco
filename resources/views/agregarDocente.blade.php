482ae32a6c24f452fbcf6dc483a46dad82e03975
nicolas britto <nicobritto21@gmail.com>
2023-10-06 22:29:25 -0300

    agregada la vista de AgregarDocente

---
+++ resources/views/agregarDocente.blade.php
@@ -0,0 +1,40 @@
+@extends('layout/plantilla')
+
+@section("tituloPagina","Crear un nuevo registro")
+
+@section('contenido')
+
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Agregar Nuevo</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
